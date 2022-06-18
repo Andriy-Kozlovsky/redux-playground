71f30e32a05eafa6d16d6b16bb06850601b4d3d3
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-06-18 18:32:14 -0300

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,5 @@
-import {useSelector} from "react-redux";
-
-import Auth from "./components/Auth";
-import Counter from "./components/Counter";
-import Header from "./components/Header";
-import UserProfile from "./components/UserProfile";
-
 function App() {
-  const isAuth = useSelector((state: any) => state.auth.isAuthenticated);
-
-  return (
-    <>
-      <Header />
-      {!isAuth && <Auth />}
-      {isAuth && <UserProfile />}
-      <Counter />
-    </>
-  );
+  return <div />;
 }
 
 export default App;
