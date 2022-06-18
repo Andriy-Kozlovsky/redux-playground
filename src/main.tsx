dfcfde1c9e69b625f2bd6bb902464809f70105b7
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-06-18 14:55:09 -0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import {Provider} from "react-redux";
 import store from "./store";
 import App from "./App";
 import "./styles.css";
-import store from "./store/index";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
