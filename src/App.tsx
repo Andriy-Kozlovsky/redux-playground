41c8348109c7d968147dcc014c5c6841891f2728
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-06-19 20:22:16 -0300

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import Form from "./components/Form";
-import List from "./components/List";
-
 function App() {
-  return (
-    <div className="app">
-      <h1>Lista de productos: Redux Toolkit</h1>
-      <Form />
-      <List />
-    </div>
-  );
+  return <div />;
 }
 
 export default App;
