28d0e6e360ec1919b1280dec98a21c5c97487960
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-06-20 20:31:28 -0300

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,5 @@
-import {useEffect} from "react";
-import {useSelector, useDispatch} from "react-redux";
-
-import Cart from "./components/Cart/Cart";
-import Layout from "./components/Layout/Layout";
-import Products from "./components/Shop/Products";
-import Notification from "./components/UI/Notification";
-import {sendCartData, fetchCartData} from "./store/cart-actions";
-
-let initial = true;
-
 function App() {
-  const dispatch = useDispatch();
-  const showCart = useSelector((state: any) => state.ui.cartIsVisible);
-  const cart = useSelector((state: any) => state.cart);
-  const notification = useSelector((state: any) => state.ui.notification);
-
-  useEffect(() => {
-    // @ts-ignore
-    dispatch(fetchCartData());
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (initial) {
-      initial = false;
-
-      return;
-    }
-    if (cart.changed) {
-      // @ts-ignore
-      dispatch(sendCartData(cart));
-    }
-  }, [cart, dispatch]);
-
-  return (
-    <>
-      {notification && (
-        <Notification
-          message={notification.message}
-          status={notification.status}
-          title={notification.title}
-        />
-      )}
-      <Layout>
-        {showCart && <Cart />}
-        <Products />
-      </Layout>
-    </>
-  );
+  return <div />;
 }
 
 export default App;
