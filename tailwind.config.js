49d4abb292ed98006250155be5efe58928dba671
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-06-20 16:57:23 -0300

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Noto Sans JP", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        badgeTxt: "#1d1d1d",
+        headerBg: "#252424",
+        cartBg: "#313131",
+        itemBg: "#575757",
+        buttonHoverBg: "#4b4b4b",
+        cian: "#1ad1b9",
+        itemTxt: "#3a3a3a",
+        productButtonTx: "#1a8ed1",
+        productButtonHover: "#1ac5d1",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
