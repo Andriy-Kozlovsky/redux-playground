b217825c9c2acae47d9e60d549aab9404b782840
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-06-20 16:54:46 -0300

    clean

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {ThemeProvider} from "@mui/material/styles";
 
-import theme from "./theme";
+import "./index.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
 );
